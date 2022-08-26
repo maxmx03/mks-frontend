7da7a627d0c211ca8fc59f6abff843c029916bac
Max Miliano <milianordelcanto@gmail.com>
2022-08-26 19:19:48 -0300

    add redux example, chackra ui example

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { Provider } from 'react-redux'
+import store from '../lib/store'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+const config = {
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  endOfLine: 'lf',
+}
+
+module.exports = config
